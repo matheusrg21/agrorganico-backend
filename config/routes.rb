564d5243076019fcda5e25af95af9efc43e655da
Pedro <pedro.sousa@spezi.com.br>
2021-10-18 22:52:16 -0300

    Changing routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@
 
 Rails.application.routes.draw do
   resources :fruits
-  resources :users
-  resources :shopping_lists
-
+  resources :users do
+    resources :shopping_lists
+  end
   post '/auth/login', to: 'authentication#login'
   get '/*a', to: 'application#not_found'
 end
